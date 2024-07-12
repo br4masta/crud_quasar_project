8ff5928d97f08cc63a62ae460eb31fb2692d82b4
br4masta <brianaldi2701@gmail.com>
2024-07-12 23:14:56 +0700

    up

---
+++ src/pages/addProject.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="row q-pa-md">
+    <div class="col-4 text-h4">Add Project</div>
+  </div>
+  <hr class="q-mx-md" />
+  <div class="q-pa-md">progress bar here</div>
+</template>
